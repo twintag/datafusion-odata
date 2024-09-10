8d434a3164f8602c64c236859c5d0acee49abf3a
hozan23 <hozan23@karyontech.net>
2024-09-10 10:38:01 +0200

    remove string msg from CollectionAddresNotAssigned error

--- src/error.rs
+++ src/error.rs
@@ -28,6 +28,6 @@ pub enum Error {
     UnsupportedFeature(String),
     #[error("Collection not found: {0}")]
     CollectionNotFound(String),
-    #[error("Collection address not assigned: {0}")]
-    CollectionAddressNotAssigned(String),
+    #[error("Collection address not assigned")]
+    CollectionAddressNotAssigned,
 }
